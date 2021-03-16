c2446faf20b4db0dfc6899d9f16ca4c74c92c5cc
Jonathan Muñoz <davidsansonii@hotmail.com>
2021-03-16 17:09:35 -0500

    ajustes a schema.sql

--- microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -8,34 +8,35 @@ create table usuario (
 
 CREATE TABLE propietario (
   id int(11) not null auto_increment,
-  nombre varchar(45) DEFAULT NULL,
-  numero_identificacion varchar(45) DEFAULT NULL,
-  telefono varchar(45) DEFAULT NULL,
-  correo varchar(45) DEFAULT NULL,
-  direccion varchar(45) DEFAULT NULL,
+  nombre varchar(45) not null,
+  numero_identificacion varchar(45) not null,
+  telefono varchar(45) not null,
+  correo varchar(45) not null,
+  direccion varchar(45) not null,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE tarifa (
   id int(11) NOT NULL AUTO_INCREMENT,
-  avaluo_minimo bigint(11) DEFAULT NULL,
-  avaluo_maximo bigint(11) DEFAULT NULL,
-  tarifa decimal(10,3) DEFAULT NULL,
-  anio int(11) DEFAULT NULL,
+  avaluo_minimo bigint(11) not null,
+  avaluo_maximo bigint(11) not null,
+  tarifa decimal(10,3) not null,
+  anio int(11) not null,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE inmueble (
   id int(11) NOT NULL AUTO_INCREMENT,
-  numero_predial bigint(11) DEFAULT NULL,
-  codigo_postal int(11) DEFAULT NULL,
-  direccion varchar(50) DEFAULT NULL,
-  area_total int(11) DEFAULT NULL,
-  area_construida int(11) DEFAULT NULL,
-  avaluo_catastral bigint(11) DEFAULT NULL,
-  id_propietario int(11) DEFAULT NULL,
+  numero_predial bigint(11) not null,
+  codigo_postal int(11) not null,
+  direccion varchar(50) not null,
+  area_total int(11) not null,
+  area_construida int(11) not null,
+  avaluo_catastral bigint(11) not null,
+  id_propietario int(11) not null,
   PRIMARY KEY (id),
-  KEY FK_I_PROPIETARIO_idx (id_propietario),
-  CONSTRAINT FK_I_PROPIETARIO FOREIGN KEY (id_propietario) REFERENCES propietario (id) ON UPDATE NO ACTION
+  foreign key (id_propietario) REFERENCES propietario(id)
 );
+
+
 
