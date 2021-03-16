b8dd00366c008af194eb784884a9cf5e8a79b455
Jonathan Muñoz <davidsansonii@hotmail.com>
2021-03-16 18:51:44 -0500

    ServicioCrearPago ajustes

--- microservicio/dominio/src/main/java/com/ceiba/pago/servicio/ServicioCrearPago.java
+++ microservicio/dominio/src/main/java/com/ceiba/pago/servicio/ServicioCrearPago.java
@@ -4,21 +4,19 @@ import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.pago.modelo.entidad.Pago;
 import com.ceiba.pago.puerto.repositorio.RepositorioPago;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 
 public class ServicioCrearPago {
 
     private final RepositorioPago repositorioPago;
 
     private static final String EL_PAGO_YA_EXISTE_EN_EL_SISTEMA = "El pago ya existe en el sistema";
-    private static final double PORCENTAJE_DESCUENTO = 10;
-    private static final double PORCENTAJE_MULTA_ANIO = 10;
-    private static final String DIA_SABADO = "Sabado";
-    private static final String DIA_DOMINGO = "Domingo";
-    private static final int ANIO_ACTUAL = Calendar.YEAR;
+    private static final LocalDate FECHA_ACTUAL = LocalDate.now();
+    private final int anio_actual;
 
     public ServicioCrearPago(RepositorioPago repositorioPago) {
         this.repositorioPago = repositorioPago;
+        this.anio_actual = FECHA_ACTUAL.getYear();
     }
 
     public Long ejecutar(Pago pago) {
@@ -28,6 +26,7 @@ public class ServicioCrearPago {
 
     private void validarExistenciaPrevia(Pago pago) {
         boolean existe = this.repositorioPago.existe(pago.getIdPropietario(), pago.getIdInmueble(), pago.getAnio());
+
         if (existe) {
             throw new ExcepcionDuplicidad(EL_PAGO_YA_EXISTE_EN_EL_SISTEMA);
         }
