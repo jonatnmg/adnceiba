e42c4fef567f1d8b6858078ea050dcc1845f5a5c
Jonathan Muñoz <davidsansonii@hotmail.com>
2021-03-16 21:20:46 -0500

    ServicioCrearPago ajustes. v3

--- microservicio/dominio/src/main/java/com/ceiba/pago/servicio/ServicioCrearPago.java
+++ microservicio/dominio/src/main/java/com/ceiba/pago/servicio/ServicioCrearPago.java
@@ -7,6 +7,7 @@ import com.ceiba.pago.puerto.repositorio.RepositorioPago;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 
 public class ServicioCrearPago {
 
@@ -15,15 +16,26 @@ public class ServicioCrearPago {
     private static final String EL_PAGO_YA_EXISTE_EN_EL_SISTEMA = "El pago ya existe en el sistema";
     private static final String NO_SE_PERMITEN_PAGOS_LOS_DIAS_SABADOS_O_DOMINGOS = "No se permiten pagos los dias sabados o domingos";
     private static final LocalDate FECHA_ACTUAL = LocalDate.now();
-    private static final int anioActual = FECHA_ACTUAL.getYear();
+    private static final int ANIO_ACTUAL = FECHA_ACTUAL.getYear();
+
+    private static final int DIA_MAXIMO_DESCUENTO = 30;
+    private static final Month MES_MAXIMO_DESCUENTO = Month.APRIL;
+
+    private static final double PORCENTAJE_DESCUENTO = 0.1;
+    private static final double PORCENTAJE_NO_DESCUENTO = 0;
 
     public ServicioCrearPago(RepositorioPago repositorioPago) {
         this.repositorioPago = repositorioPago;
     }
 
     public Long ejecutar(Pago pago) {
+
         this.validarDiasParaPago(pago);
+        double descuentoAplicar = this.validarAplicarDescuento(pago);
+        System.out.println("descuentoAplicar = " + descuentoAplicar);
+
         this.validarExistenciaPrevia(pago);
+
         return this.repositorioPago.crear(pago);
     }
 
@@ -41,6 +53,28 @@ public class ServicioCrearPago {
         if (fechaPago.getDayOfWeek() == DayOfWeek.SATURDAY || fechaPago.getDayOfWeek() == DayOfWeek.SUNDAY) {
             throw new ExcepcionCrearPago(NO_SE_PERMITEN_PAGOS_LOS_DIAS_SABADOS_O_DOMINGOS);
         }
+    }
+
+    private double validarAplicarDescuento(Pago pago) {
+        LocalDate fechaPago = pago.getFechaPago();
+        int anio_a_pagar = pago.getAnio();
+
+        if (ANIO_ACTUAL == anio_a_pagar) {
+            LocalDate fechaMaximaParaDescuento = this.fechaMaximaDescuento();
+            boolean aplicarDescuento = this.validarFechasParaDescuento(fechaPago, fechaMaximaParaDescuento);
+
+            return aplicarDescuento ? PORCENTAJE_DESCUENTO : PORCENTAJE_NO_DESCUENTO;
+        }
+
+        return PORCENTAJE_NO_DESCUENTO;
+    }
+
+    private LocalDate fechaMaximaDescuento() {
+        return LocalDate.of(ANIO_ACTUAL, MES_MAXIMO_DESCUENTO, DIA_MAXIMO_DESCUENTO);
+    }
+
+    private boolean validarFechasParaDescuento(LocalDate fechaPago, LocalDate fechaMaximaDescuento) {
+        return !fechaPago.isAfter(fechaMaximaDescuento);
     }
 
 }
