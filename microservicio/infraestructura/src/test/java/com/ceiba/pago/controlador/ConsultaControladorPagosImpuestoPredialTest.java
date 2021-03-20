657d7a3d2a5d13b4d187e63f6c9c28460f46599e
Jonathan Muñoz <davidsansonii@hotmail.com>
2021-03-20 18:41:29 -0500

    Test integracion

---
+++ microservicio/infraestructura/src/test/java/com/ceiba/pago/controlador/ConsultaControladorPagosImpuestoPredialTest.java
@@ -0,0 +1,33 @@
+package com.ceiba.pago.controlador;
+
+import com.ceiba.ApplicationMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ApplicationMock.class)
+@WebMvcTest(ConsultaControladorPago.class)
+public class ConsultaControladorPagosImpuestoPredialTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void listar() throws Exception {
+        // arrange
+
+        // act - assert
+        mockMvc.perform(get("/pagos")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
