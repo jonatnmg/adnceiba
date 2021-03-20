151d3abc07773d42298a151ee981701e4ca39b7f
Jonathan Muñoz <davidsansonii@hotmail.com>
2021-03-20 18:54:27 -0500

    SE ELIMINA USUARIOS

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -16,30 +16,11 @@ import com.ceiba.tarifa.puerto.repositorio.RepositorioTarifa;
 import com.ceiba.tarifa.servicio.ServicioActualizarTarifa;
 import com.ceiba.tarifa.servicio.ServicioCrearTarifa;
 import com.ceiba.tarifa.servicio.ServicioEliminarTarifa;
-import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
-import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
-import com.ceiba.usuario.servicio.ServicioCrearUsuario;
-import com.ceiba.usuario.servicio.ServicioEliminarUsuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
-
-    @Bean
-    public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioCrearUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioEliminarUsuario servicioEliminarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioEliminarUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioActualizarUsuario(repositorioUsuario);
-    }
 
     @Bean
     public ServicioCrearPropietario servicioCrearPropietario(RepositorioPropietario repositorioPropietario) {
